9613781ad041ea514b3446c0ecafcb85f2824dbe
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-02-22 17:09:05 +0000

    Book class and repo

---
+++ PhilsBookshop/WebApplication3/Book.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhilsBookshop.Web
+{
+    public class Book
+    {
+        [Key]
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ PhilsBookshop/WebApplication3/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PhilsBookshop.Web
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> List();
+        Book Get(string ISBN);
+        void Update(Book book);
+        void Delete(string ISBN);
+        void Insert(Book book);
+    }
+}
